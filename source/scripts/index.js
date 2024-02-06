0c770b8934f8fb971772897fbd87e063b1ddf5c7
rychagoff <coming.sites@gmail.com>
2024-02-07 00:24:01 +0400

    Добавляет логику бегунка

--- source/scripts/index.js
+++ source/scripts/index.js
@@ -22,3 +22,70 @@ navToggle.addEventListener('click', () => {
 });
 
 map.style.zIndex = '0';
+
+const range = document.querySelector('.example__range');             // Ищем бегунок
+const rangeParent = range.offsetParent;                              // Ищем слайдер
+const catBefore = document.querySelector('.example__image--before'); // Ищем блок "До"
+const catAfter = document.querySelector('.example__image--after');   // Ищем блок "После"
+
+const rangeParentWidth = rangeParent.offsetWidth;                    // Читаем ширину слайдера
+
+range.addEventListener('mousedown', (evt) => {
+  dragging = true;                                                   // Бегунок может двигаться
+  evt.preventDefault();                                              // Выключаем запуск выделения (действие браузера)
+
+  let rangeStyle = window.getComputedStyle(range);                   // Ищем CSS-стили бегунка
+  let leftX = parseInt(rangeStyle.getPropertyValue('--x'));          // Читаем переменную "--x" положения бегунка
+  let catBeforeStyle = window.getComputedStyle(catBefore);           // Ищем CSS-стили блока "До"
+  let clipBefore = catBeforeStyle.getPropertyValue('--clip-before'); // Читаем переменную "--clip-before"
+  let catAfterStyle = window.getComputedStyle(catAfter);             // Ищем CSS-стили блока "После"
+  let clipAfter = catAfterStyle.getPropertyValue('--clip-after');    // Читаем переменную "--clip-after"
+
+  let rangeParentLeft = rangeParent.getBoundingClientRect().left;    // Читаем расстояние от края окна до слайдера
+  let rangeLeft = range.getBoundingClientRect().left;                // Читаем расстояние от края окна до бегунка
+
+  console.log('Текущее положение (left) бегунка = ' + leftX+'%');
+  console.log('Текущее положение курсора = ' + evt.clientX+'px');
+  console.log('Расстояние от левого края окна до слайдера = ' + rangeParentLeft+'px');
+  console.log('Расстояние от левого края окна до бегунка = ' + rangeLeft+'px');
+  console.log('Расстояние от курсора до левого края бегунка = ' + (evt.clientX - rangeLeft)+'px');
+  console.log('----------');
+
+  document.addEventListener('mousemove', onMouseMove);               // Функция движения мыши
+  document.addEventListener('mouseup', onMouseUp);                   // Функция отпускания кнопки мыши
+
+  function onMouseMove(evt) {
+    if (!dragging) return;                                           // Если бегунок двигается, то функция выполняется
+
+    console.log('Новое положение курсора = ' + evt.clientX+'px');
+    console.log('Расстояние от края слайдера до нового положения курсора = ' + (evt.clientX - rangeParentLeft) +'px');
+
+    leftX = (evt.clientX - rangeParentLeft) / rangeParentWidth;      // Считаем соотношение нового положения курсора
+                                                                     // с шириной слайдера
+
+    // Курсор вышел слева из слайдера => оставить бегунок в его границах.
+    if (leftX < 0) {
+      leftX = 0;
+    }
+    // Курсор вышел справа из слайдера => оставить бегунок в его границах.
+    if (leftX > 1) {
+      leftX = 1;
+    }
+
+    leftX = leftX.toFixed(6) * 100;                           // Сокращаем значение до 6 знаков после запятой
+    console.log(leftX);                                       // Выводим новое значение в консоль
+
+    clipAfter = `0 0 0 ${leftX}%`;                            // Определяем новое значение переменной "--clip-after"
+    clipBefore = `0 ${100 - leftX}% 0 0`;                     // Определяем новое значение переменной "--clip-before"
+
+    range.style.setProperty("--x", `${leftX}%`);              // Пишем новое положение бегунка в переменную "--x"
+    catAfter.style.setProperty('--clip-after', clipAfter);    // Пишем новую обрезку кота в блоке "После"
+    catBefore.style.setProperty('--clip-before', clipBefore); // Пишем новую обрезку кота в блоке "До"
+  }
+
+  function onMouseUp() {
+    document.removeEventListener('mouseup', onMouseUp);       // Завершаем событие нажатия кнопки мыши
+    document.removeEventListener('mousemove', onMouseMove);   // Завершаем событие движения мыши
+    dragging = false;                                         // Бегунок теперь не двигается
+  }
+});
